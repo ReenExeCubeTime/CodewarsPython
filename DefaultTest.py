1dc93fcef6377417bb57eba0ca0ff80ac6501c8a
ReenExe <ipourri@gmail.com>
2017-03-08 19:51:11 +0200

    compare array

--- DefaultTest.py
+++ DefaultTest.py
@@ -3,7 +3,7 @@ import unittest
 
 class DefaultTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(1, 1)
+        self.assertEqual([9923, 9931, 9941, 9967], [9923, 9931, 9941, 9967])
 
 
 unittest.main()
