7dcbb0e7056ed4dff7f9bfc0223a81cbd7ca653e
ReenExe <ipourri@gmail.com>
2017-03-08 20:52:30 +0200

    debug (#1)

--- DefaultTest.py
+++ DefaultTest.py
@@ -5,7 +5,8 @@ from Prime import Prime
 class DefaultTest(unittest.TestCase):
     def testPrime(self):
         prime = Prime()
-        self.assertEqual(prime.between(9000, 10000), [9923, 9931, 9941, 9967])
+        print(prime.backwards(9000, 10000))
+        # self.assertEqual(prime.between(9000, 10000), [9923, 9931, 9941, 9967])
 
 
 unittest.main()
--- Prime.py
+++ Prime.py
@@ -1,4 +1,34 @@
+import math
+
+
 class Prime(object):
 
-    def between(self, start, stop):
-        return None
+    def backwards(self, start, stop):
+        to = stop + 1
+        map = range(0, to)
+
+        max = math.floor(math.sqrt(stop)) + 1
+
+        delimiter = 2
+
+        while delimiter < max:
+            complex = delimiter
+
+            while complex < to:
+                map[complex] = 0
+                complex += delimiter
+
+            delimiter += 1
+
+        slice = map[start:to]
+
+        primes = set(slice) - set([0])
+
+        backwards = []
+        for prime in primes:
+            reverse = int(str(prime)[::-1])
+            if map[reverse]:
+                print([prime, reverse])
+
+
+
