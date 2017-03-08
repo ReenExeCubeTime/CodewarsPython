ee744bdb0abdb6f61a50737c77cc7dc172a74214
ReenExe <ipourri@gmail.com>
2017-03-08 19:56:54 +0200

    require file

--- DefaultTest.py
+++ DefaultTest.py
@@ -1,8 +1,11 @@
 import unittest
 
+from Prime import Prime
 
 class DefaultTest(unittest.TestCase):
-    def test(self):
+    def testPrime(self):
+        prime = Prime()
+        self.assertEqual(prime.fromRange(9000, 10000), None)
         self.assertEqual([9923, 9931, 9941, 9967], [9923, 9931, 9941, 9967])
 
 
---
+++ Prime.py
@@ -0,0 +1,4 @@
+class Prime(object):
+
+    def fromRange(self, start, stop):
+        return None
