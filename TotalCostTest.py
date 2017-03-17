7af95f2277a3a63c85fa2b607a8775583b6f0694
ReenExe <ipourri@gmail.com>
2017-03-17 23:05:04 +0200

    test before code (#2)

--- TotalCostTest.py
+++ TotalCostTest.py
@@ -7,5 +7,6 @@ class TotalCostTest(unittest.TestCase):
         cost = TotalCost()
         costs = {'socks': 5, 'shoes': 60, 'sweater': 30}
         self.assertEqual(cost.calculate(costs, ['socks', 'shoes'], 0.09), 70.85)
+        self.assertEqual(cost.calculate(costs, ['shirt', 'shoes'], 0.05), 63.00)
 
 unittest.main()
