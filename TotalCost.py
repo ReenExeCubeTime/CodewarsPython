4c84dd44081ce27f14b14c814def91aef0c3d470
ReenExe <ipourri@gmail.com>
2017-03-17 23:05:17 +0200

    pass tests (#2)

--- TotalCost.py
+++ TotalCost.py
@@ -3,5 +3,6 @@ class TotalCost(object):
     def calculate(self, costs, items, tax):
         sum = 0
         for item in items:
-            sum += costs[item]
+            if item in costs:
+                sum += costs[item]
         return sum + sum * tax
