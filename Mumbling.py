87b8e0d2d9273acc6b4d58316dd26a53cd23697d
ReenExe <ipourri@gmail.com>
2017-03-17 23:25:42 +0200

    Mumbling #3

---
+++ Mumbling.py
@@ -0,0 +1,4 @@
+class Mumbling(object):
+
+    def accum(self, string):
+        pass
---
+++ MumblingTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+from Mumbling import Mumbling
+
+class MumblingTest(unittest.TestCase):
+    def testPrime(self):
+        mumbling = Mumbling()
+        self.assertEqual(mumbling.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+        self.assertEqual(mumbling.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+        self.assertEqual(mumbling.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+        self.assertEqual(mumbling.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+        self.assertEqual(mumbling.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+
+unittest.main()
