3fbd22e7c0f2ad471895821a7d0c46c986e884e4
ReenExe <ipourri@gmail.com>
2017-03-17 22:56:16 +0200

    rename

--- PrimeTest.py
+++ PrimeTest.py
@@ -2,7 +2,7 @@ import unittest
 
 from Prime import Prime
 
-class DefaultTest(unittest.TestCase):
+class PrimeTest(unittest.TestCase):
     def testPrime(self):
         prime = Prime()
         self.assertEqual(prime.backwards(9900, 10000), [9923, 9931, 9941, 9967])
