227eefab96beaaedd06e669ffc09f14807d7c275
ReenExe <ipourri@gmail.com>
2017-03-18 23:49:14 +0200

    pass tests (#12)

--- DuplicateEncoder.py
+++ DuplicateEncoder.py
@@ -1,3 +1,15 @@
+from collections import defaultdict
+
+
 class DuplicateEncoder(object):
     def encode(self, word):
-        return word
+        map = defaultdict(int)
+
+        for char in word:
+            map[char.lower()] += 1
+
+        result = []
+        for char in word:
+            result.append(')' if map[char.lower()] > 1 else '(')
+
+        return ''.join(result)
