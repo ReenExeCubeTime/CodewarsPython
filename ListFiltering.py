c34f13ee9f8cd857c25464598c0d3153ad64a70b
ReenExe <ipourri@gmail.com>
2017-03-18 23:27:15 +0200

    best practice (#11)

--- ListFiltering.py
+++ ListFiltering.py
@@ -1,8 +1,3 @@
 class ListFiltering(object):
     def filter(self, l):
-        result = []
-        for item in l:
-            if type(item) is int:
-                result.append(item)
-
-        return  result
+        return [i for i in l if isinstance(i, int)]
