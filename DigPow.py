5394af6b33aeb2df241ab24d4be61e0fbfebd642
ReenExe <ipourri@gmail.com>
2017-03-18 22:52:17 +0200

    pass tests (#9)

--- DigPow.py
+++ DigPow.py
@@ -1,3 +1,11 @@
 class DigPow(object):
-    def calculate(self, number):
-        return number
+    def calculate(self, n, p):
+        digits = str(n)
+
+        sum = 0
+        for digitString in digits:
+            digit = int(digitString)
+            sum += digit ** p
+            p += 1
+
+        return sum / n if sum % n == 0 else -1
