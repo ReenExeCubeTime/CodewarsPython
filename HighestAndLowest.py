a343e72a8caee7058a54c11d390b49b3b8fa9ded
ReenExe <ipourri@gmail.com>
2017-03-19 00:15:32 +0200

    pass tests (#13)

--- HighestAndLowest.py
+++ HighestAndLowest.py
@@ -1,3 +1,18 @@
+import string
+
+
 class HighestAndLowest(object):
     def find(self, numbers):
-        return numbers
+        number_list = numbers.split(' ')
+
+        first = int(number_list[0])
+        max_number = first
+        min_number = first
+
+        for number in number_list:
+            number = int(number)
+
+            max_number = max(max_number, number)
+            min_number = min(min_number, number)
+
+        return str(max_number) + ' ' + str(min_number)
--- HighestAndLowestTest.py
+++ HighestAndLowestTest.py
@@ -6,13 +6,13 @@ from HighestAndLowest import HighestAndLowest
 class HighestAndLowestTest(unittest.TestCase):
     def test(self):
         highestAndLowest = HighestAndLowest()
-        self.assertEqual(highestAndLowest.find("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
-        self.assertEqual(highestAndLowest.find("1 -1"), "1 -1");
-        self.assertEqual(highestAndLowest.find("1 1"), "1 1");
-        self.assertEqual(highestAndLowest.find("-1 -1"), "-1 -1");
-        self.assertEqual(highestAndLowest.find("1 -1 0"), "1 -1");
-        self.assertEqual(highestAndLowest.find("1 1 0"), "1 0");
-        self.assertEqual(highestAndLowest.find("-1 -1 0"), "0 -1");
-        self.assertEqual(highestAndLowest.find("42"), "42 42");
+        self.assertEqual(highestAndLowest.find("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214")
+        self.assertEqual(highestAndLowest.find("1 -1"), "1 -1")
+        self.assertEqual(highestAndLowest.find("1 1"), "1 1")
+        self.assertEqual(highestAndLowest.find("-1 -1"), "-1 -1")
+        self.assertEqual(highestAndLowest.find("1 -1 0"), "1 -1")
+        self.assertEqual(highestAndLowest.find("1 1 0"), "1 0")
+        self.assertEqual(highestAndLowest.find("-1 -1 0"), "0 -1")
+        self.assertEqual(highestAndLowest.find("42"), "42 42")
 
 unittest.main()
