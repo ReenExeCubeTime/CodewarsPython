c71b9d7759aec342fe46323382ee97dd05f22074
ReenExe <ipourri@gmail.com>
2017-03-18 23:07:07 +0200

    pass tests (#10)

--- ReplaceAlphabetPosition.py
+++ ReplaceAlphabetPosition.py
@@ -1,3 +1,15 @@
+import string
+
+
 class ReplaceAlphabetPosition(object):
+
     def replace(self, text):
-        return text
+        result = []
+        letters = string.ascii_uppercase
+        shift = ord('A') - 1
+        for letter in text:
+            letter = letter.upper()
+            if letter in letters:
+                result.append(str(ord(letter) - shift))
+
+        return ' '.join(result)
