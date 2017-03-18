11b3ed5a810a2b4e6db8b3781173adf86a6b23aa
ReenExe <ipourri@gmail.com>
2017-03-18 23:51:17 +0200

    use best practice (#12)

--- DuplicateEncoder.py
+++ DuplicateEncoder.py
@@ -1,15 +1,4 @@
-from collections import defaultdict
-
-
 class DuplicateEncoder(object):
     def encode(self, word):
-        map = defaultdict(int)
-
-        for char in word:
-            map[char.lower()] += 1
-
-        result = []
-        for char in word:
-            result.append(')' if map[char.lower()] > 1 else '(')
-
-        return ''.join(result)
+        lower = word.lower()
+        return "".join(["(" if lower.count(char) == 1 else ")" for char in lower])
