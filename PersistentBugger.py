bf50756fa92f6eb8e663bf82633b61c3171c2cc5
ReenExe <ipourri@gmail.com>
2017-03-19 04:22:54 +0200

    try self (#16)

--- PersistentBugger.py
+++ PersistentBugger.py
@@ -1,3 +1,8 @@
 class PersistentBugger(object):
     def persistence(self, n):
+        while n > 10:
+            digits = map(int, str(n).split(''))
+            n = 1
+            for digit in digits:
+                n *= digit
         return n
