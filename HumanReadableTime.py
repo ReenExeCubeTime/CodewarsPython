fbf38ed6e558f3f47daee4980423f60fc6734dca
ReenExe <ipourri@gmail.com>
2017-03-19 05:18:02 +0200

    pass tests (#17)

--- HumanReadableTime.py
+++ HumanReadableTime.py
@@ -1,3 +1,9 @@
 class HumanReadableTime(object):
+    def __pad(self, number):
+        return str(number) if number > 9 else "0" + str(number)
     def make(self, seconds):
-        return seconds
+        s = seconds % 60
+        minutes = seconds // 60
+        m = minutes % 60
+        h = min(minutes // 60, 99)
+        return ":".join(map(self.__pad, [h, m, s]))
