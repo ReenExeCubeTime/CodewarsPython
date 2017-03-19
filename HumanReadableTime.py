19befd5934318849e6bce60bd1c9ade8018eb3a6
ReenExe <ipourri@gmail.com>
2017-03-19 05:21:39 +0200

    best practice (#17)

--- HumanReadableTime.py
+++ HumanReadableTime.py
@@ -1,9 +1,7 @@
 class HumanReadableTime(object):
-    def __pad(self, number):
-        return str(number) if number > 9 else "0" + str(number)
     def make(self, seconds):
         s = seconds % 60
         minutes = seconds // 60
         m = minutes % 60
-        h = min(minutes // 60, 99)
-        return ":".join(map(self.__pad, [h, m, s]))
+        h = minutes // 60
+        return '{:02}:{:02}:{:02}'.format(h, m, s)
